a957d18b65a90f0765f25b93aee1f126276b1ef1
Joppe Aarts <joppe@apestaartje.info>
2012-05-15 21:28:38 +0200

    development

---
+++ javascript/jquery.slider.js
@@ -0,0 +1,23 @@
+/*global jQuery*/
+
+(function ($) {
+    'use strict';
+
+    var Slider;
+
+    Slider = (function () {
+        return {
+            init: function () {
+            },
+            next: function () {
+            },
+            previous: function () {
+            }
+        };
+    }());
+
+    $.fn.slider = function () {
+        return this.each(function () {
+        });
+    };
+}(jQuery));
