59a8e1c720496207a48431ac58f422a6ce8835cc
Joppe Aarts <joppe@apestaartje.info>
2014-02-12 22:12:28 +0100

    fixed the slider controls a bit to work with the new slider

--- src/jquery.slider-controls.js
+++ src/jquery.slider-controls.js
@@ -8,95 +8,82 @@
     'use strict';
 
     var Controls,
-        default_options = {
+        defaultOptions = {
             next: 'li.next a',
             previous: 'li.previous a',
-            number: null
+            numbers: null
         };
 
-    Controls = (function () {
-        var Controls;
+    Controls = function ($container, $slider, options) {
+        this.$container = $container;
+        this.$slider = $slider;
 
-        Controls = function ($container, $slider, options) {
-            this.$container = $container;
-            this.$slider = $slider;
+        this.$next = this.$container.find(options.next);
+        this.$previous = this.$container.find(options.previous);
+        this.$numbers = this.$container.find(options.numbers);
 
-            this.$next = this.$container.find(options.next);
-            this.$previous = this.$container.find(options.previous);
-            this.$numbers = this.$container.find(options.number);
+        this.addEventListeners();
 
-            this.addEventListeners();
+        this.$slider.trigger('refreshstatus');
+    };
+    Controls.prototype = {
+        addEventListeners: function () {
+            console.log('addEventListeners');
+            this.$next.on('click', $.proxy(function (event) {
+                event.preventDefault();
 
-            this.$slider.trigger('sliderRefreshStatus');
-        };
-        Controls.prototype = {
-            addEventListeners: function () {
-                var self = this;
+                this.$slider.trigger('next');
+            }, this));
 
-                this.$next.on({
-                    click: function (event) {
-                        event.preventDefault();
+            this.$previous.on('click', $.proxy(function (event) {
+                event.preventDefault();
 
-                        self.$slider.trigger('sliderForward', 1);
-                    }
-                });
-                this.$previous.on({
-                    click: function (event) {
-                        event.preventDefault();
+                this.$slider.trigger('previous');
+            }, this));
 
-                        self.$slider.trigger('sliderReverse', 1);
-                    }
-                });
+            this.$numbers.on('click', $.proxy(function (event) {
+                var index = this.$numbers.index(event.target);
 
-                this.$numbers.on({
-                    click: function (event) {
-                        var index = self.$numbers.index($(this));
+                event.preventDefault();
 
-                        event.preventDefault();
+                this.$slider.trigger('moveTo', index);
+            }, this));
 
-                        self.$slider.trigger('sliderMoveTo', index);
-                    }
-                });
+            this.$slider.on({
+                status: $.proxy(function (event, status) {
+                    this.update(status);
+                }, this)
+            });
+        },
 
-                this.$slider.on({
-                    sliderStatus: function (event, sliderStatus) {
-                        self.update(sliderStatus);
-                    },
-                    sliderAfterChange: function (event, sliderStatus) {
-                        self.update(sliderStatus);
-                    }
-                });
-            },
-
-            update: function (sliderStatus) {
-                if (sliderStatus.options.loopMode === 'none') {
-                    if (this.$next.size() > 0) {
-                        if (sliderStatus.index.active >= sliderStatus.index.max) {
-                            this.$next.addClass('disabled');
-                        } else {
-                            this.$next.removeClass('disabled');
-                        }
-                    }
-                    if (this.$previous.size() > 0) {
-                        if (sliderStatus.index.active === 0) {
-                            this.$previous.addClass('disabled');
-                        } else {
-                            this.$previous.removeClass('disabled');
-                        }
+        update: function (status) {
+            /*
+            if (status.options.loopMode === 'none') {
+                if (this.$next.size() > 0) {
+                    if (status.index.active >= status.index.max) {
+                        this.$next.addClass('disabled');
+                    } else {
+                        this.$next.removeClass('disabled');
                     }
                 }
-                if (this.$numbers.size() > 0) {
-                    this.$numbers.filter('.active.').removeClass('active');
-                    this.$numbers.eq(sliderStatus.normalizeIndex(sliderStatus.index.activeIndex)).removeClass('active');
+                if (this.$previous.size() > 0) {
+                    if (status.index.active === 0) {
+                        this.$previous.addClass('disabled');
+                    } else {
+                        this.$previous.removeClass('disabled');
+                    }
                 }
             }
-        };
+            if (this.$numbers.size() > 0) {
+                this.$numbers.filter('.active.').removeClass('active');
+                this.$numbers.eq(status.normalizeIndex(status.index.activeIndex)).removeClass('active');
+            }
+            */
+        }
+    };
 
-        return Controls;
-    }());
-
-    $.fn.sliderControls = function ($slider, config) {
-        var options = $.extend(default_options, config === undefined ? {} : config);
+    $.fn.sliderControls = function ($slider, options) {
+        options = $.extend({}, defaultOptions, options || {});
 
         return this.each(function () {
             var controls = new Controls($(this), $slider, options);
