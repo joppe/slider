92a53933ad5beb4433ed1d09af9796a42be00f39
Joppe Aarts <joppe@apestaartje.info>
2014-03-16 12:08:34 +0100

    iterator methods added

--- src/jquery.iterator.js
+++ src/jquery.iterator.js
@@ -1,13 +1,17 @@
-/*global define*/
+/*global jQuery*/
 
 /**
  * @author Joppe Aarts <joppe@apestaartje.info>
  */
 
-define(function () {
+(function ($) {
     'use strict';
 
-    return function (array) {
+    /**
+     * @param array
+     * @returns {{count: count, current: current, key: key, next: next, previous: previous, reset: reset, seek: seek, valid: valid}}
+     */
+    $.iterator = function (array) {
         var position = 0;
 
         return {
@@ -32,21 +36,42 @@ define(function () {
                 return position;
             },
 
+            /**
+             * @returns {$.iterator}
+             */
             next: function () {
                 position += 1;
                 return this;
             },
 
+            /**
+             * @returns {$.iterator}
+             */
+            previous: function () {
+                position -= 1;
+                return this;
+            },
+
+            /**
+             * @returns {$.iterator}
+             */
             reset: function () {
                 position = 0;
                 return this;
             },
 
+            /**
+             * @param newPosition
+             * @returns {$.iterator}
+             */
             seek: function (newPosition) {
                 position = newPosition;
                 return this;
             },
 
+            /**
+             * @returns {boolean}
+             */
             valid: function () {
                 var valid = false;
 
@@ -55,13 +80,7 @@ define(function () {
                 }
 
                 return valid;
-            },
-
-            each: function (callback, context) {
-                for (this.reset(); this.valid(); this.next()) {
-                    callback.call(context, this.current(), this.key());
-                }
             }
         };
     };
-});
+}(jQuery));
